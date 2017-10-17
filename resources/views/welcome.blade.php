32ef59ebcd1f6fab831081e189ae7099057d878e
zhumingzhen <itdream53@gmail.com>
2017-10-18 01:31:02 +0800

    8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel88661
+                    Laravel111
                 </div>
 
                 <div class="links">

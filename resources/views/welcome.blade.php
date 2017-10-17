ab3b619029006aa356aa176d804d104074562673
zhumingzhen <itdream53@gmail.com>
2017-10-18 01:42:07 +0800

    11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel666
+                    Laravel123
                 </div>
 
                 <div class="links">

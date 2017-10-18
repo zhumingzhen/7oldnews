93caace217e9d8be092693f7ac5493a968c37030
zhumingzhen <itdream53@gmail.com>
2017-10-18 22:38:22 +0800

    news model fillable

--- app/News.php
+++ app/News.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     protected $table = 'news';
+
+    protected $fillable = ['title','time','src','category','pic','content','url','weburl'];
 }

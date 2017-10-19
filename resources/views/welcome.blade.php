50cbd29ed8c1c5f5a7c215074f7fad97adf92f76
zhumingzhen <itdream53@gmail.com>
2017-10-19 12:28:08 +0800

    new branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel9999999
+                    Laravel
                 </div>
 
                 <div class="links">

b84624dffd446d344daa219bb6c113fd947ba15a
zhumingzhen <itdream53@gmail.com>
2017-10-22 19:04:31 +0800

    ceshi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 3
+                    Laravel 4
                 </div>
 
                 <div class="links">

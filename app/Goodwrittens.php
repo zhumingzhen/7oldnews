73bffb41b6cce4d40296d3b93d867121a94a4d04
zhumingzhen <itdream53@gmail.com>
2017-10-22 23:02:56 +0800

    查询数据

--- app/Goodwrittens.php
+++ app/Goodwrittens.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Goodwrittens extends Model
 {
-    //
+    protected $table = 'goodwrittens';
 }
--- app/Http/Controllers/GoodwrittenController.php
+++ app/Http/Controllers/GoodwrittenController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Goodwrittens;
 
 class GoodwrittenController extends Controller
 {
     public function index()
     {
+        return Goodwrittens::get();
         return view('goodwritten');
     }
 

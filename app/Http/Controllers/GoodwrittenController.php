ce2946909009ec7f78a225fb7303784e60a4e137
zhumingzhen <itdream53@gmail.com>
2017-10-23 22:21:21 +0800

    增加加解密方法

--- app/Http/Controllers/GoodwrittenController.php
+++ app/Http/Controllers/GoodwrittenController.php
@@ -10,6 +10,10 @@ class GoodwrittenController extends Controller
     public function index()
     {
         $goodwrittens = Goodwrittens::get();
+        echo $enstr = $this->pwdEncode('zmz110');
+        echo '-------';
+        echo $this->pwdDecode($enstr);
+        exit;
         return view('goodwritten',compact('goodwrittens'));
     }
 
@@ -19,6 +23,40 @@ class GoodwrittenController extends Controller
         $res = ['code'=>200,'msg'=>'请求成功','data'=>$operation];
         return $res;
     }
+
+    /**
+     * 简单对称加密算法之加密
+     * @param String $string 需要加密的字串
+     * @param String $skey 加密EKY
+     * @author Anyon Zou <zoujingli@qq.com>
+     * @date 2013-08-13 19:30
+     * @update 2014-10-10 10:10
+     * @return String
+     */
+    public function pwdEncode($string = '', $skey = 'goodwritten') {
+        $strArr = str_split(base64_encode($string));
+        $strCount = count($strArr);
+        foreach (str_split($skey) as $key => $value)
+            $key < $strCount && $strArr[$key].=$value;
+        return str_replace(array('=', '+', '/'), array('O0O0O', 'o000o', 'oo00o'), join('', $strArr));
+    }
+    /**
+     * 简单对称加密算法之解密
+     * @param String $string 需要解密的字串
+     * @param String $skey 解密KEY
+     * @author Anyon Zou <zoujingli@qq.com>
+     * @date 2013-08-13 19:30
+     * @update 2014-10-10 10:10
+     * @return String
+     */
+    public function pwdDecode($string = '', $skey = 'goodwritten') {
+        $strArr = str_split(str_replace(array('O0O0O', 'o000o', 'oo00o'), array('=', '+', '/'), $string), 2);
+        $strCount = count($strArr);
+        foreach (str_split($skey) as $key => $value)
+            $key <= $strCount  && isset($strArr[$key]) && $strArr[$key][1] === $value && $strArr[$key] = $strArr[$key][0];
+        return base64_decode(join('', $strArr));
+    }
+
 
     // 接受post数据
     public function query1()

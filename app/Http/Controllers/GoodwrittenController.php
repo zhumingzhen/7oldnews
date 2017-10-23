c1e2b623f05a1ade6cb96ee65f193a5f8d49a7d2
zhumingzhen <itdream53@gmail.com>
2017-10-23 23:00:45 +0800

    解密密文

--- app/Http/Controllers/GoodwrittenController.php
+++ app/Http/Controllers/GoodwrittenController.php
@@ -10,18 +10,20 @@ class GoodwrittenController extends Controller
     public function index()
     {
         $goodwrittens = Goodwrittens::get();
-        echo $this->pwdDecode('egmo1o6dMwTrEiwt');exit;
-        echo $enstr = $this->pwdEncode('zmz110');
-        echo '-------';
-        echo $this->pwdDecode($enstr);
-        exit;
         return view('goodwritten',compact('goodwrittens'));
     }
 
     public function query(Request $request)
     {
         $operation = $request->input('operation');
-        $res = ['code'=>200,'msg'=>'请求成功','data'=>$operation];
+        $GoodwrittenData = Goodwrittens::where('operation',$operation)->first();
+        if ($GoodwrittenData){
+            $ciphertext = $GoodwrittenData['ciphertext'];
+            $dePassword = $this->pwdDecode($ciphertext,$operation);
+            $res = ['code'=>200,'msg'=>'请求成功','data'=>$dePassword];
+        }else{
+            $res = ['code'=>400,'msg'=>'或许是密钥不对呢','data'=>''];
+        }
         return $res;
     }
 
@@ -29,12 +31,9 @@ class GoodwrittenController extends Controller
      * 简单对称加密算法之加密
      * @param String $string 需要加密的字串
      * @param String $skey 加密EKY
-     * @author Anyon Zou <zoujingli@qq.com>
-     * @date 2013-08-13 19:30
-     * @update 2014-10-10 10:10
      * @return String
      */
-    public function pwdEncode($string = '', $skey = 'goodwritten1') {
+    public function pwdEncode($string = '', $skey = 'goodwritten') {
         $strArr = str_split(base64_encode($string));
         $strCount = count($strArr);
         foreach (str_split($skey) as $key => $value)
@@ -45,12 +44,9 @@ class GoodwrittenController extends Controller
      * 简单对称加密算法之解密
      * @param String $string 需要解密的字串
      * @param String $skey 解密KEY
-     * @author Anyon Zou <zoujingli@qq.com>
-     * @date 2013-08-13 19:30
-     * @update 2014-10-10 10:10
      * @return String
      */
-    public function pwdDecode($string = '', $skey = 'goodwritten1') {
+    public function pwdDecode($string = '', $skey = 'goodwritten') {
         $strArr = str_split(str_replace(array('O0O0O', 'o000o', 'oo00o'), array('=', '+', '/'), $string), 2);
         $strCount = count($strArr);
         foreach (str_split($skey) as $key => $value)

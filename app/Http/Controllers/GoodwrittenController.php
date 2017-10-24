32237b3d6bc92478441ff12e70c3045300abb36c
zhumingzhen <itdream53@gmail.com>
2017-10-24 20:48:52 +0800

    插入数据库

--- app/Http/Controllers/GoodwrittenController.php
+++ app/Http/Controllers/GoodwrittenController.php
@@ -34,10 +34,8 @@ class GoodwrittenController extends Controller
     public function add(Request $request)
     {
         if ($request->isMethod('post')){
-            $input = $request->except('_token'); //同上
-            dd($input);
-            $data = $request->all();
-            dd($data);
+//            $data = $request->all();
+            $data = $request->except('_token'); //同上
             Goodwrittens::create($data);
         }
         return view('gwadd');

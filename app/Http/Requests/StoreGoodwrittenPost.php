0fad16e94d7c5e2197c577571d919737718cf126
zhumingzhen <itdream53@gmail.com>
2017-10-28 15:09:46 +0800

    required messages

--- app/Http/Requests/StoreGoodwrittenPost.php
+++ app/Http/Requests/StoreGoodwrittenPost.php
@@ -30,4 +30,15 @@ class StoreGoodwrittenPost extends FormRequest
             'operation' => 'bail|required'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'describe.required' => 'describe 是必须的',
+            'account.required'  => 'account 是必须的',
+            'password.required'  => 'password 是必须的',
+            'password.min'  => 'password 最少6为',
+            'operation.required'  => 'operation 是必须的',
+        ];
+    }
 }

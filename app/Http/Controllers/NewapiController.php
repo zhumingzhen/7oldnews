3510dff6504ee2d3a873352510b9bbe9651ee11a
zhumingzhen <itdream53@gmail.com>
2017-11-02 19:21:16 +0800

    required messages

--- app/Http/Controllers/NewapiController.php
+++ app/Http/Controllers/NewapiController.php
@@ -27,7 +27,7 @@ class NewapiController extends Controller
                 $save['content']=$list['content'];
                 $save['url']=$list['url'];
                 $save['weburl']=$list['weburl'];
-                News::create($save);  // 如果save失败，返回false；如果成功，返回model。
+                echo News::create($save);  // 如果save失败，返回false；如果成功，返回model。
             }
         }
     }

2a4cddb8ff0022a00a28cf8e742dffdde4aa9916
zhumingzhen <itdream53@gmail.com>
2018-02-08 23:01:11 +0800

    required messages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>7oldnews-1i11</title>
+        <title>7oldnews-111</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">

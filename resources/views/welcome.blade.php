7e5eec1026e55572c0d973e97cf1b98708fb89b0
zhumingzhen <itdream53@gmail.com>
2018-02-11 10:11:36 +0800

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>7oldnews-111</title>
+        <title>7oldnews</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
